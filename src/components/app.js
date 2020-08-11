2aa82665c0b4be600405a2c2521f0d4dd902a669
canti <ljdemar91@gmail.com>
2020-08-11 16:14:38 -0500

    ccreated recent posts and searchbar components and used in the home component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
-import Logo from "./logo"
+import React, { Component } from "react";
+
+import Logo from "./logo";
+import Searchbar from "./searchbar";
+import RecentPosts from "./recent-posts";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className="app">
         <Logo />
+        <Searchbar />
+        <RecentPosts />
       </div>
     );
   }
---
+++ src/components/searchbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Searchbar extends Component {
+  render() {
+    return (
+     <form>
+         <input placeholder="search DailySmarty" />
+     </form>
+    );
+  }
+}
+
+export default Searchbar;
