d38a305f18efde429550f5e201f49524eb87822e
canti <ljdemar91@gmail.com>
2020-08-11 22:47:44 -0500

    connected recent posts to redux

--- src/components/home.js
+++ src/components/home.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 import Logo from "./logo";
 import SearchBar from "./searchbar";
-import RecentPosts from "./recent-posts";
+import RecentPosts from "./recentPosts";
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="app">
+      <div>
         <Logo />
         <SearchBar />
         <RecentPosts />
