3005187f5a08986b99dd766b8975735e3b1742a7
canti <ljdemar91@gmail.com>
2020-08-15 01:36:47 -0500

    added SET_RESULTS_POSTS reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,18 +1,26 @@
-import { SET_RECENT_POSTS } from "../actions/types";
+import { SET_RECENT_POSTS, SET_RESULTS_POSTS } from "../actions/types";
+import results from "../components/results";
 
 const INIT_STATE = {
-  posts: [],
+  resultsPosts: [],
   recentPosts: [],
 };
 
 export default function (state = INIT_STATE, action) {
   switch (action.type) {
     case SET_RECENT_POSTS:
-        const recentPosts = action.payload;
-        return {
-                ...state,
-                recentPosts
-            }
+      const recentPosts = action.payload;
+      return {
+        ...state,
+        recentPosts,
+      };
+
+    case SET_RESULTS_POSTS:
+      const resultsPosts = action.payload;
+      return {
+        ...state,
+        resultsPosts,
+      };
     default:
       return state;
   }
