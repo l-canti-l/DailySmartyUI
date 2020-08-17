741f1743e83cf8dc200770c7ff853e81ea84f733
canti <ljdemar91@gmail.com>
2020-08-17 02:46:16 -0500

    added hover to post links in post.js

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
+import AnimateHeight from "react-animate-height";
 
 class Post extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      height: 0,
+    };
+  }
+
   renderTopics() {
     let topics = this.props.associated_topics.map((topic, index) => {
       return (
@@ -30,20 +39,29 @@ class Post extends Component {
     if (this.props.type == "recent") {
       return (
         <li className="recent-post">
-          <div className="recent-post__title">
-            {this.props.title}
-          </div>
-          <div className="recent-post__topics">
-            {this.renderTopics()}
-          </div>
+          <div className="recent-post__title">{this.props.title}</div>
+          <div className="recent-post__topics">{this.renderTopics()}</div>
         </li>
-      )
+      );
     } else if (this.props.type == "result") {
       return (
         <li className="result-post">
           <div className="result-post__topics">{this.renderTopics()}</div>
-          <div className="result-post__title">{this.props.title}</div>
-          <div className="result-post__links">{this.renderLinks()}</div>
+          <div className="result-post__title">
+            <a 
+            href={this.props.url_for_post}
+            onMouseEnter={() => this.setState({height: 70})}
+            onMouseLeave={() => this.setState({height: 0})}
+            >
+              {this.props.title}
+            </a>
+          </div>
+          <AnimateHeight 
+            duration={500} 
+            height={this.state.height}
+          >
+            <div className="result-post__links">{this.renderLinks()}</div>
+          </AnimateHeight>
         </li>
       );
     }
