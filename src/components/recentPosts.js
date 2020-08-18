bb14bd27a78c25520d7754aa7eb2a26e0c28e7ce
canti <ljdemar91@gmail.com>
2020-08-17 20:20:42 -0500

    stled recent posts component

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -25,7 +25,7 @@ class RecentPosts extends Component {
   render() {
     return (
       <div className="recent-posts">
-        <div className="recent-posts-wrapper">
+        <div className="recent-posts__wrapper">
           <div className="recent-posts__heading">Recent Posts</div>
           <ul className="recent-posts__posts">
             {this.renderPosts()}
